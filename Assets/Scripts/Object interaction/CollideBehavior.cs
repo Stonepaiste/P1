5741f99da5e92133c2d8c902e0c30662408661fe
Bünyamin Ensar Erman <erensar@outlook.dk>
2023-11-13 15:39:03 +0100

    Update CollideBehavior.cs

--- Assets/Scripts/Object interaction/CollideBehavior.cs
+++ Assets/Scripts/Object interaction/CollideBehavior.cs
@@ -7,9 +7,42 @@ public class CollideBehavior : MonoBehaviour
 {
 
     public bool HasBottleTrash;
+    public bool HasOilBarrelTrash;
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.gameObject.name == "Flaske_Prop")
+        {
+            Debug.Log("Trash collision");
+            Destroy(other.gameObject); // Destroy the collided Trash object
+            HasBottleTrash = true;
+
+        }
+        else if (other.gameObject.name == "Quest1" && HasBottleTrash)
+
+        {
+            Debug.Log("Quest collision");
+            HasBottleTrash = false;
+
+        }
+
+
+        if (other.gameObject.name == "OilBarrel_Prop")
+        {
+            Debug.Log("Trash collision");
+            Destroy(other.gameObject); // Destroy the collided Trash object
+            HasOilBarrelTrash = true;
+
+        }
+        else if (other.gameObject.name == "Quest2" && HasBottleTrash)
+
+        {
+            Debug.Log("Quest collision");
+            HasOilBarrelTrash = false;
+
+        }
+
         if (other.gameObject.name == "Flaske_Prop")
         {
             Debug.Log("Trash collision");
@@ -24,7 +57,10 @@ public class CollideBehavior : MonoBehaviour
             HasBottleTrash = false;
 
         }
+
     }
+
+
 
 }
 
