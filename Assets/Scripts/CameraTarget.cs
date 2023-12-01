addbf828be0e136004e31803bd41ad288afb471c
Marcelkress <Marcel@kress.dk>
2023-12-01 10:56:23 +0100

    ss

--- Assets/Scripts/CameraTarget.cs
+++ Assets/Scripts/CameraTarget.cs
@@ -8,12 +8,18 @@ public class CameraTarget : MonoBehaviour
     public Vector3 offset;
     private Vector3 followVelocity = Vector3.zero;
     public float followTime;
+    public float minYPos = -35;
 
     private void LateUpdate()
     {
         Vector3 nextTarget = target.position + offset;
 
         Vector3 nextposition = Vector3.SmoothDamp(transform.position, nextTarget, ref followVelocity, followTime);
+
+        if (nextposition.y < minYPos)
+        {
+            nextposition.y = minYPos;
+        }
 
         transform.position = nextposition;
     }
