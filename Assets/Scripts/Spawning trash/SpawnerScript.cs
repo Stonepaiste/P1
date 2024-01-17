e2bd8cfe12045c913261b6bb0350d18c85a5094e
Marcelkress <Marcel@kress.dk>
2024-01-17 12:25:26 +0100

    kommentar til spawnscript

--- Assets/Scripts/Spawning trash/SpawnerScript.cs
+++ Assets/Scripts/Spawning trash/SpawnerScript.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class spawning : MonoBehaviour
 {
-    public GameObject[] trash;
-    public Vector3 spawnValues;
+    public GameObject[] trash;              //Liste med trash objekter
+    public Vector3 spawnValues;             //Område hvor trash kan spawne
     public float spawnMostWait;
     public float spawnLeastWait;
 
     private void Start()
     {
-        StartCoroutine(StartTrash());
+        StartCoroutine(StartTrash());           
     }
 
     // Start is called before the first frame update
@@ -19,15 +19,16 @@ public class spawning : MonoBehaviour
     {
         while (true)
         {
-            SpawnTrash();
-            yield return new WaitForSeconds(Random.Range(spawnLeastWait, spawnMostWait));
+            SpawnTrash();       //Kalder spawntrash methoden
+            yield return new WaitForSeconds(Random.Range(spawnLeastWait, spawnMostWait));           //Venter random antal sekunder
         }
     }
 
     private void SpawnTrash()
     {
-        int randTrash = Random.Range(0, trash.Length);
+        int randTrash = Random.Range(0, trash.Length);      //Vælger et tilfædligt stykke skrald
+        //LAver en random spawnposition
         Vector3 randomSpawnPosition = new Vector3(Random.Range(transform.position.x -spawnValues.x,transform.position.x + spawnValues.x), transform.position.y + spawnValues.y, 1);
-        Instantiate(trash[randTrash], randomSpawnPosition, Quaternion.identity);
+        Instantiate(trash[randTrash], randomSpawnPosition, Quaternion.identity);     //Spawner en et stykke skrald på den random position
     }
 }
